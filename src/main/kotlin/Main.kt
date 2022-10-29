5b1e3fc70e4f45b9be134a62703b6215fb6283a3
CodeNinja-tech <elo48858@gmail.com>
2022-10-29 23:08:46 +0100

    Created Float and Double Data Types

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+import kotlin.reflect.typeOf
+
+fun main(args: Array<String>) {
+  // Floating Point Numbers are used for storing decimals
+  val myNumber: Float = 2.5F // This is a float
+  val doubleNumber: Double = 1.5
+
+  println("Float: $myNumber, Double: $doubleNumber")
+}
